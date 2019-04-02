9c4cd1d914055211fab626c6c8b53c35fdd1492d
Dennis Pfleger <dennis.pfleger@web.de>
2019-04-02 18:23:18 +0200

    Return -1

--- solarsystem/solarsystem/stellar_object.h
+++ solarsystem/solarsystem/stellar_object.h
@@ -115,4 +115,8 @@ public:
 		double distance = sqrt(pow((other->x - this->x), 2) + pow((other->y - this->y), 2) + pow((other->z - this->z), 2)); 
 		return distance; 
 	}
+
+	double calculate_velocity() {
+		return 1; 
+	}
 };
